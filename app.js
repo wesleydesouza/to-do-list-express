d779862e9c6d8a65254a83732b5342938b6d35b7
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-05-02 12:31:15 -0300

    middleware

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-const express = require("express");
+const express = require("express");//import tando o express
+
+const app = express();
+
+//criando uma rota
+app.get("/", (req, res) => {
+    res.send("<h1>Minha Lista de Tarefas</h1>");
+});
+
+//criando um MiddleWare
+app.use(express.json());
+
+const log = (req, res, next) =>{
+    console.log(req.body);
+    console.log(`Data: ${Date.now()}`);
+    next();
+}
+
+app.use(log);
+
+//devolvendo um json
+app.get("/json", (req, res) =>{
+    
+    res.json({title: "Tarefa X", done: true});
+})
+
+//criando um servidor
+app.listen(3000, () => {
+    console.log("Servidor foi iniciado");
+})
